4e6392c6b49658f36cbd90ea2bc59a82c022187c
ciroBorrelli <c.borrelli@sky-media.it>
2015-12-29 16:36:51 +0100

    CIRO: new class according to the use of List<Hit>

--- src/test/java/it/skymedia/idolTunnel/test/IdolOEMConnectionTest.java
+++ src/test/java/it/skymedia/idolTunnel/test/IdolOEMConnectionTest.java
@@ -2,6 +2,7 @@ package it.skymedia.idolTunnel.test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import it.skymedia.idolTunnel.Hit;
 import it.skymedia.idolTunnel.IdolOEMConnection;
 
 import java.util.ArrayList;
@@ -12,6 +13,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * This @Test class is not part of the WS-Client, it doesnt access the endpoint
+ * <br/>This is a test class which directly uses IdolOEMConnection class methods
+ * <br/>This is intended for JUnit and Maven tests
+ * <br/>
+ * THIS ALWAYS SHOULD RETURN ALL-GREEN SIGNALS
+ */
 public class IdolOEMConnectionTest {
 	
 	static IdolOEMConnection ic; 
@@ -50,30 +58,6 @@ public class IdolOEMConnectionTest {
 		//fail("Not yet implemented");
 	}
 	
-//	@Test
-//	public void testAutnResponseToDocument() throws Exception {
-//		
-//		Map<String, String> pList = new HashMap<String, String>();
-//		pList.put("action", "query");
-//		pList.put("text", "*");
-//		pList.put("anylanguage", "true");
-//		pList.put("maxresults", "25");
-//		pList.put("databasematch", "");
-//		pList.put("combine", "simple");
-//		pList.put("outputencoding", "utf8");
-//		pList.put("totalResults", "true");
-//		
-//		String expectedFirstNode = "autnresponse";
-//		String expectedFirstNodeAttr = "http://schemas.autonomy.com/aci/";
-//		String autnresponse = ic.aciRequest(pList, "xml");
-//		Document documentResult = ic.responseToXMLDocument(autnresponse);
-//		
-//		assertTrue("Document shold start with <autnresponse> node!", documentResult.getFirstChild().getNodeName().equals(expectedFirstNode));
-//		assertTrue("Document first node should contains attributes(1)!", documentResult.getFirstChild().hasAttributes());
-//		assertTrue("Document <autnresponse> Attribute should be : http://schemas.autonomy.com/aci/ !", ((Element)documentResult.getFirstChild()).getAttribute("xmlns:autn").equals(expectedFirstNodeAttr));
-//
-//	}
-	
 	@Test
 	public void testResponseNodeFromDocument() throws Exception {
 		
@@ -111,11 +95,90 @@ public class IdolOEMConnectionTest {
 		int size = 25;
 		String autnresponse = ic.aciRequest(pList, "xml");
 		//Document documentResult = ic.responseToXMLDocument(autnresponse);
-		ArrayList<HashMap<String, String>> listHits = ic.getQueryHitsMap(autnresponse);
+		ArrayList<Hit> listHits = ic.getQueryHitsMap(autnresponse);
 		
 		assertTrue("Result List Size should be 25!", listHits.size()== size);
 		
 	}
 	
-
+	@Test
+	public void testGetSpellCheckFieldsFromDocument() throws Exception {
+		
+		Map<String, String> pList = new HashMap<String, String>();
+		pList.put("action", "query");
+		pList.put("text", "latitnte indagto ompuatto");
+		pList.put("anylanguage", "true");
+		pList.put("print", "noresults");
+		pList.put("spellcheck", "true");
+		pList.put("SpellCheckAlphaNumeric", "false");
+		pList.put("outputencoding", "utf8");
+		
+		String autnresponse = ic.aciRequest(pList, "xml");
+		//Document documentResult = ic.responseToXMLDocument(autnresponse);
+		String[] spellCheckFields = ic.getSpellCheckFields(autnresponse);
+		
+		assertTrue("Result should contains two not empty fields", 
+				spellCheckFields.length > 1 
+				&& spellCheckFields[1].length() > 0
+				&& spellCheckFields[0].length() > 0 
+		);
+		
+		//debug;
+		System.out.println("\n\tSPELLCHECK FIELDS :\n" + 
+				spellCheckFields[0] + "\n" + spellCheckFields[1]
+		);
+	}
+	
+	@Test
+	public void testGetSpellCheckFieldsFromEmptyQuery() throws Exception {
+		
+		Map<String, String> pList = new HashMap<String, String>();
+		pList.put("action", "query");
+		pList.put("text", "*");
+		pList.put("anylanguage", "true");
+		pList.put("print", "noresults");
+		pList.put("spellcheck", "true");
+		pList.put("SpellCheckAlphaNumeric", "false");
+		pList.put("outputencoding", "utf8");
+		
+		String autnresponse = ic.aciRequest(pList, "xml");
+		//Document documentResult = ic.responseToXMLDocument(autnresponse);
+		String[] spellCheckFields = ic.getSpellCheckFields(autnresponse);
+		
+		assertTrue("Result should contains two not empty fields", 
+				spellCheckFields.length > 1 
+				&& spellCheckFields[1].length() < 1
+				&& spellCheckFields[0].length() > 0 
+		);
+		
+		//debug;
+		System.out.println("\n\tSPELLCHECK FIELDS :\n" + 
+				spellCheckFields[0] + "\n" + spellCheckFields[1]
+		);
+	}
+	
+	@Test
+	public void testGetHitIndexedContent() throws Exception {
+		
+		Map<String, String> pList = new HashMap<String, String>();
+		pList.put("action", "query");
+		pList.put("text", "*");
+		pList.put("maxresults", "10");
+		pList.put("anylanguage", "true");
+		pList.put("print", "indexText");
+		pList.put("outputencoding", "utf8");
+		
+		String autnresponse = ic.aciRequest(pList, "xml");
+		ArrayList<Hit> hits = ic.getQueryHitsNoDocumentMap(autnresponse);
+		
+		assertTrue("Result should contains the autn:content not-empty field", 
+				hits.size() <= 10 
+				&& ic.getHitIndexedContent(hits.get(0).getDreFields()).length() > 0
+		);
+		
+		//debug;
+		System.out.println("\n\tINDEXED CONTENT:\n" + 
+				ic.getHitIndexedContent(hits.get(0).getDreFields())
+		);
+	}
 }
