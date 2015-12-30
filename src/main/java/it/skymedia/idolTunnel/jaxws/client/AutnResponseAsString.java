46b4dd879e3f1f6c9ce3922ea7ba2dcff447ee88
ciroBorrelli <c.borrelli@sky-media.it>
2015-12-30 16:01:17 +0100

    updated JAXB auto-generated ws-client

---
+++ src/main/java/it/skymedia/idolTunnel/jaxws/client/AutnResponseAsString.java
@@ -0,0 +1,112 @@
+
+package it.skymedia.idolTunnel.jaxws.client;
+
+import java.util.HashMap;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java per autnResponseAsString complex type.
+ * 
+ * <p>Il seguente frammento di schema specifica il contenuto previsto contenuto in questa classe.
+ * 
+ * <pre>
+ * &lt;complexType name="autnResponseAsString">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="arg0">
+ *           &lt;complexType>
+ *             &lt;complexContent>
+ *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *                 &lt;sequence>
+ *                   &lt;element name="entry" maxOccurs="unbounded" minOccurs="0">
+ *                     &lt;complexType>
+ *                       &lt;complexContent>
+ *                         &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *                           &lt;sequence>
+ *                             &lt;element name="key" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *                             &lt;element name="value" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *                           &lt;/sequence>
+ *                         &lt;/restriction>
+ *                       &lt;/complexContent>
+ *                     &lt;/complexType>
+ *                   &lt;/element>
+ *                 &lt;/sequence>
+ *               &lt;/restriction>
+ *             &lt;/complexContent>
+ *           &lt;/complexType>
+ *         &lt;/element>
+ *         &lt;element name="arg1" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "autnResponseAsString", propOrder = {
+    "arg0",
+    "arg1"
+})
+public class AutnResponseAsString {
+
+    @XmlElement(required = true)
+    protected HashMap<String, String> arg0;
+    protected String arg1;
+
+    /**
+     * Recupera il valore della proprieta arg0.
+     * 
+     * @return
+     *     possible object is
+     *     {@link AutnResponseAsString.Arg0 }
+     *     
+     */
+    public HashMap<String, String> getArg0() {
+        return arg0;
+    }
+
+    /**
+     * Imposta il valore della proprieta arg0.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link AutnResponseAsString.Arg0 }
+     *     
+     */
+    public void setArg0(HashMap<String, String> value) {
+        this.arg0 = value;
+    }
+
+    /**
+     * Recupera il valore della proprieta arg1.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getArg1() {
+        return arg1;
+    }
+
+    /**
+     * Imposta il valore della proprieta arg1.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setArg1(String value) {
+        this.arg1 = value;
+    }
+
+}
---
+++ src/main/java/it/skymedia/idolTunnel/jaxws/client/AutnResponseAsStringResponse.java
@@ -0,0 +1,62 @@
+
+package it.skymedia.idolTunnel.jaxws.client;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java per autnResponseAsStringResponse complex type.
+ * 
+ * <p>Il seguente frammento di schema specifica il contenuto previsto contenuto in questa classe.
+ * 
+ * <pre>
+ * &lt;complexType name="autnResponseAsStringResponse">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="return" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "autnResponseAsStringResponse", propOrder = {
+    "_return"
+})
+public class AutnResponseAsStringResponse {
+
+    @XmlElement(name = "return")
+    protected String _return;
+
+    /**
+     * Recupera il valore della proprieta return.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getReturn() {
+        return _return;
+    }
+
+    /**
+     * Imposta il valore della proprieta return.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setReturn(String value) {
+        this._return = value;
+    }
+
+}
